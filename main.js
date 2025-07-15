4a28e3e5f15d5e0871b75150f76fcf7adf5489d6
ogurgen25 <46230022+ogurgen25@users.noreply.github.com>
2025-07-15 21:15:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,185 +1,28 @@
-// 1. Harita kurulumu
-const map = L.map("map", {
-  center: [40.915, 38.321],
+const map = new maplibregl.Map({
+  container: 'map',
+  style: 'https://demotiles.maplibre.org/style.json',
+  center: [38.3215, 40.915], // Giresun kampüsüne odak
   zoom: 17,
-  minZoom: 16,
-  maxZoom: 22,
-  maxBounds: [
-    [40.912, 38.317],
-    [40.918, 38.326]
-  ],
-  maxBoundsViscosity: 1.0
+  pitch: 45,
+  bearing: -17.6,
+  antialias: true
 });
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap',
-  noWrap: true,
-  maxZoom: 22,
-  minZoom: 16
-}).addTo(map);
 
-// 2. Katman tanımları (fakülte hariç hepsi)
-const layersConfig = [
-  { name: "Kütüphane",        url: "data/KÜTÜPHANE_FeaturesToJSON.json",     color: "#673ab7" },
-  { name: "Laboratuvar",      url: "data/LABORATUVARR_FeaturesToJSON.json",  color: "#e91e63" },
-  { name: "Laboratuvar-2",    url: "data/LABORATUVARR_FeaturesToJSON1.json", color: "#f44336" },
-  { name: "Lojmanlar",        url: "data/LOJMANLAR_FeaturesToJSON.json",     color: "#009688" },
-  { name: "Rektörlük",        url: "data/REKTÖRLÜK_FeaturesToJSON.json",     color: "#3f51b5" },
-  { name: "Spor Salonu",      url: "data/SPOR_SALONUU_FeaturesToJSON.json",  color: "#4caf50" },
-  { name: "Üniversite Birimleri", url: "data/ÜNİVERSİTE_BİRİMLER_Features.json", color: "#ffc107" },
-  { name: "Yemekhane",        url: "data/YEMEKHANEE_FeaturesToJSON.json",    color: "#ff9800" },
-  { name: "Yeşil Alan",       url: "data/YESİLALAN_FeaturesToJSON.json",     color: "#8bc34a" },
-  { name: "Yol Ağı",          url: "data/YOL_AGI.json",                      color: "#212121", type: "line" },
-  { name: "Kapı / Giriş",     url: "data/KAPI_GİRİS.json",                   color: "#d84315", type: "point" },
-  { name: "Bankamatik",       url: "data/BANKAMATİK_FeaturesToJSON.json",    color: "#00796b" },
-  { name: "Güvenlik",         url: "data/GÜVENLİK_FeaturesToJSON.json",      color: "#000000" }
-];
+map.on('load', () => {
+  map.addSource('fakulte', {
+    'type': 'geojson',
+    'data': 'data/FAKULTE_3D.json'
+  });
 
-// 3. Katmanları haritaya ekle
-let overlayLayers = {};
-layersConfig.forEach(layer => {
-  fetch(layer.url).then(res => res.json()).then(data => {
-    let leafletLayer;
-    if (layer.type === "line") {
-      leafletLayer = L.geoJSON(data, {
-        style: { color: layer.color, weight: 4, dashArray: "8, 8" }
-      }).bindPopup(layer.name);
-    } else if (layer.type === "point") {
-      leafletLayer = L.geoJSON(data, {
-        pointToLayer: (feature, latlng) => L.circleMarker(latlng, {
-          radius: 7,
-          fillColor: layer.color,
-          color: "#222",
-          weight: 2,
-          opacity: 1,
-          fillOpacity: 0.9
-        }),
-        onEachFeature: (feature, l) => {
-          let info = `<b>${layer.name}</b>`;
-          if (feature.properties.KAPI_ADI) info += `<br>${feature.properties.KAPI_ADI}`;
-          if (feature.properties.ADI) info += `<br>${feature.properties.ADI}`;
-          l.bindPopup(info);
-        }
-      });
-    } else {
-      leafletLayer = L.geoJSON(data, {
-        style: { color: layer.color, fillOpacity: 0.5, weight: 2 },
-        onEachFeature: (feature, l) => {
-          let info = `<b>${layer.name}</b>`;
-          if (feature.properties.ADI) info += `<br>${feature.properties.ADI}`;
-          if (feature.properties.FAKULTE_ADI) info += `<br>${feature.properties.FAKULTE_ADI}`;
-          l.bindPopup(info);
-        }
-      });
-    }
-    leafletLayer.addTo(map);
-    overlayLayers[layer.name] = leafletLayer;
-    if (Object.keys(overlayLayers).length === layersConfig.length) {
-      L.control.layers(null, overlayLayers, { collapsed: false }).addTo(map);
+  map.addLayer({
+    'id': 'fakulte-3d',
+    'type': 'fill-extrusion',
+    'source': 'fakulte',
+    'paint': {
+      'fill-extrusion-color': '#0074D9',
+      'fill-extrusion-height': ['get', 'YUKSEKLIK'],
+      'fill-extrusion-base': 0,
+      'fill-extrusion-opacity': 0.85
     }
   });
 });
-
-// 4. FAKÜLTE-BÖLÜM-PERSONEL DROPDOWN VE VURGULAMA
-let fakulteGeoJSON, bolumler, personeller;
-let highlightLayer;
-
-// FAKÜLTE KATMANI HARİTADA HER ZAMAN ÇİZİLİ KALSIN
-fetch("data/FAKULTE.json")
-  .then(res => res.json())
-  .then(data => {
-    fakulteGeoJSON = data;
-    drawFakultePolygons();
-    initializeFakulteDropdown();
-  });
-
-// Dropdownlar için diğer jsonları yükle
-fetch("data/bolumler.json").then(res => res.json()).then(data => bolumler = data);
-fetch("data/personel.json").then(res => res.json()).then(data => personeller = data);
-
-// Fakülteleri haritaya ekle (her zaman göster)
-function drawFakultePolygons() {
-  L.geoJSON(fakulteGeoJSON, {
-    style: {
-      color: "#003366",
-      fillColor: "#66ccff",
-      weight: 2,
-      fillOpacity: 0.5
-    },
-    onEachFeature: (feature, layer) => {
-      const fakulteAdi = feature.properties.FAKULTE_ADI || feature.properties.ADI || "";
-      layer.bindPopup(`<b>${fakulteAdi}</b>`);
-      layer.on("click", () => {
-        highlightFakulte(feature.properties.FAKULTE_ID);
-        document.getElementById("fakulteSec").value = feature.properties.FAKULTE_ID;
-        triggerBolumDropdown(feature.properties.FAKULTE_ID);
-      });
-    }
-  }).addTo(map);
-}
-
-// Fakülte dropdownunu doldur
-function initializeFakulteDropdown() {
-  const fakulteDropdown = document.getElementById("fakulteSec");
-  fakulteDropdown.innerHTML = "<option value=''>Fakülte Seç</option>";
-  fakulteGeoJSON.features.forEach(f => {
-    fakulteDropdown.add(new Option(f.properties.FAKULTE_ADI, f.properties.FAKULTE_ID));
-  });
-  fakulteDropdown.disabled = false;
-  fakulteDropdown.addEventListener("change", e => {
-    triggerBolumDropdown(e.target.value);
-    highlightFakulte(e.target.value);
-  });
-}
-
-// Bölüm dropdownunu doldur
-function triggerBolumDropdown(fakulteID) {
-  const bolumDropdown = document.getElementById("bolumSec");
-  bolumDropdown.innerHTML = "<option value=''>Bölüm Seç</option>";
-  document.getElementById("personelSec").innerHTML = "<option value=''>Personel Seç</option>";
-  bolumDropdown.disabled = false;
-  document.getElementById("personelSec").disabled = true;
-  bolumler.filter(b => b.FAKULTE_ID === fakulteID).forEach(b => {
-    bolumDropdown.add(new Option(b.BOLUM_ADI, b.BOLUM_ID));
-  });
-  bolumDropdown.addEventListener("change", e => {
-    triggerPersonelDropdown(fakulteID, e.target.value);
-  }, { once: true });
-}
-
-// Personel dropdownunu doldur
-function triggerPersonelDropdown(fakulteID, bolumID) {
-  const personelDropdown = document.getElementById("personelSec");
-  personelDropdown.innerHTML = "<option value=''>Personel Seç</option>";
-  personelDropdown.disabled = false;
-  personeller.filter(p => p.FAKULTE_ID === fakulteID && p.BOLUM_ID === bolumID).forEach(p => {
-    personelDropdown.add(new Option(p.AD_SOYAD, p.PERSONEL_ID));
-  });
-  personelDropdown.addEventListener("change", e => {
-    const secilenID = e.target.value;
-    const personel = personeller.find(p => p.PERSONEL_ID === secilenID);
-    if (personel) {
-      highlightFakulte(fakulteID, true, personel);
-    }
-  }, { once: true });
-}
-
-// Seçili fakülteyi vurgula ve odağı oraya al
-function highlightFakulte(fakulteID, zoom = true, personel = null) {
-  if (highlightLayer) {
-    map.removeLayer(highlightLayer);
-  }
-  const fakulte = fakulteGeoJSON.features.find(f => f.properties.FAKULTE_ID === fakulteID);
-  if (fakulte) {
-    highlightLayer = L.geoJSON(fakulte, {
-      style: { color: "#ff5722", fillColor: "#ffd180", weight: 5, fillOpacity: 0.7 }
-    }).addTo(map);
-    if (zoom) map.fitBounds(highlightLayer.getBounds());
-    if (personel) {
-      const bounds = highlightLayer.getBounds();
-      L.popup()
-        .setLatLng(bounds.getCenter())
-        .setContent(`<b>${personel.AD_SOYAD}</b><br>${personel.UNVAN}`)
-        .openOn(map);
-    }
-  }
-}
